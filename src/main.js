4520e835073fe7be8f37ddd1659fb40196410b2c
Senj0ugahara <kamizu00@mail.ru>
2023-10-15 17:56:19 +0300

    create project and deploy full application

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import {createPinia} from 'pinia'
+import { createApp } from 'vue'
+
+import App from './App.vue'
+
+import './assets/main.css'
+
+const app = createApp(App);
+
+app
+  .use(createPinia())
+  .mount('#app')
---
+++ src/stores/MovieStore.js
@@ -0,0 +1,39 @@
+import {defineStore} from 'pinia'
+import { ref, computed, watch } from 'vue'
+
+export const useMovieStore = defineStore('movieStore', () => {
+  const movies = ref([]);
+  const activeTab = ref(2);
+
+  const moviesInLocalStorage = localStorage.getItem('movies')
+  if (moviesInLocalStorage) {
+    movies.value = JSON.parse(moviesInLocalStorage)._value;
+  }
+
+  const watchedMovies = computed(() => movies.value.filter(el => el.isWatched))
+
+  const totalCountMovies = computed(() => movies.value.length);
+
+  const setActiveTab = id => activeTab.value = id;
+
+  const toggleWatched = id => {
+    const idx = movies.value.findIndex(el => el.filmId === id);
+    movies.value[idx].isWatched = !movies.value[idx].isWatched;
+  }
+
+  const deleteMovie = id => movies.value = movies.value.filter(el => el.filmId !== id);
+
+  watch(
+    () => movies,
+    (state) => {
+      localStorage.setItem('movies', JSON.stringify(state))
+    },
+    {
+      deep: true
+    }
+  )
+
+  return {
+    movies, activeTab, watchedMovies, totalCountMovies, setActiveTab, toggleWatched, deleteMovie
+  }
+})
